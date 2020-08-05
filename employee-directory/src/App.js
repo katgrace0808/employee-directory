30e83256e76bc0b878de2f7e811b401f7321a6c2
Kathryn Teall <katgrace0808@gmail.com>
2020-08-05 16:14:03 -0700

    Initial commit

---
+++ employee-directory/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+function App() {
+    return (
+    //   <Router>
+        <div>
+          <Navbar />
+          {/* <Wrapper>
+            <Route exact path="/" component={About} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/discover" component={Discover} />
+            <Route exact path="/search" component={Search} />
+          </Wrapper>
+          <Footer /> */}
+        </div>
+    //   {/* </Router> */}
+    );
+  }
+  
+
+export default App;
---
+++ employee-directory/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Navbar() {
+    return (
+    <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">Employee Directory</a>
+    </nav>
+    )
+}
+
+export default Navbar;
